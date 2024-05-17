7a177b37887074a1158e831cdf1fce1845c7a10b
Pavl751 <104076884+Pavl751@users.noreply.github.com>
2024-05-17 22:09:30 +0300

    up

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.slider').slick({
+        adaptiveHeight:true,
+        slidesToShow: 3,
+        slidesToScroll:2,
+        speed: 1000,
+        easing:'ease',
+        touchThreshold:10,
+        // waitForAnimate:false
+            
+    });
+});
